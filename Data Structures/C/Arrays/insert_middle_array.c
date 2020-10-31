a0bdf15f5b4b566635f810f9046d875423ca6187
pamodyaDR <52200380+pamodyaDR@users.noreply.github.com>
2020-10-31 00:45:14 -0700

    Update insert_middle_array.c

--- Data Structures/C/Arrays/insert_middle_array.c
+++ Data Structures/C/Arrays/insert_middle_array.c
@@ -3,12 +3,13 @@
 #define MAX 20
 int con;
 void main(void){
+		//con value means condition value to terminate the program
+	printf("\nInput the con value : ");
+	scanf("%d",&con);
 	do{
 	int i,pos,val,array[MAX]={1,4,9,11,34,20};
 	int size=6;
-	//con value means condition value to terminate the program
-	printf("\nInput the con value : ");
-	scanf("%d",&con);
+
 	
 	printf("before inserting : ");
 	
@@ -29,6 +30,7 @@ void main(void){
 	for(i=0;i<size;i++){
 		printf(" %d ",array[i]);
 		}
+		printf("\n\n");
+		con=con-1; //decrese the con value
 	}while(con!=-1);
-	return 0;
 }
